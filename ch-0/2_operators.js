7adaf1c3c7f858c1550f3eae0abf5e315c73a7eb
Konduru, Jayasekhar <jayasekhar.konduru@cdk.com>
2016-08-03 12:34:28 +0530

    operators

---
+++ ch-0/2_operators.js
@@ -0,0 +1,38 @@
+/**
+ * Created by kondurj on 8/3/16.
+ */
+
+var sum = 20+20;
+
+var difference = 20-20;
+
+var multiply = 20*20;
+
+var concat = 'in'+'dia';
+
+var divide = 30 / 10;
+
+var identityTrue = 20 === 20;
+
+var identityFalse = 20 === 10;
+
+console.log('\n**********************************');
+
+console.log(sum);
+
+console.log(difference);
+
+console.log(multiply);
+
+console.log(concat);
+
+console.log(divide);
+
+console.log(identityTrue);
+
+console.log(identityFalse);
+
+console.log('**********************************');
+
+
+
