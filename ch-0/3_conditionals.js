71cb2e78f906c88981aa94ae1efa61d8d37bc113
Konduru, Jayasekhar <jayasekhar.konduru@cdk.com>
2016-08-03 12:48:18 +0530

    conditionals

---
+++ ch-0/3_conditionals.js
@@ -0,0 +1,20 @@
+/**
+ * Created by kondurj on 8/3/16.
+ */
+var country = 'india';
+
+console.log('\n**********************************');
+
+if(country === 'india') {
+    console.log('Welcome to the great country INDIA!!!!')
+} else {
+    console.log('Welcome!!!')
+}
+
+if(true === false) {
+    console.log('You are a liar');
+} else {
+    console.log('you are the truthful person');
+}
+
+console.log('**********************************');
