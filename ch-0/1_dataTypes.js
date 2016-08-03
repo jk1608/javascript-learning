cbf5e86d6772bd32d926aec16181d2d9403f6c79
Konduru, Jayasekhar <jayasekhar.konduru@cdk.com>
2016-08-03 12:14:25 +0530

    data types
    data types

---
+++ ch-0/1_dataTypes.js
@@ -0,0 +1,31 @@
+/**
+ * Created by kondurj on 8/3/16.
+ */
+
+var boolean = true;
+
+var string = 'hello';
+
+var number =  2020;
+
+var array = ['a', 'b', 'c'];
+
+var object = new Object();
+
+object.name = 'car';
+
+object.model = 'alto';
+
+object.make = 2016;
+
+console.log(boolean);
+
+console.log(string);
+
+console.log(number);
+
+console.log(array);
+
+console.log(object);
+
+
