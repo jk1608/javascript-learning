64dad1a3a882b9290c392a0f9ff38bef8d7d4e40
Konduru, Jayasekhar <jayasekhar.konduru@cdk.com>
2016-08-04 11:56:07 +0530

    sample function

---
+++ ch-0/4_function.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kondurj on 8/4/16.
+ */
+
+function add(arg1, arg2) {
+    return arg1 + arg2;
+}
+
+console.log('\n**********************************');
+console.log(add(20, 20));
+console.log(add(40, -20));
+console.log('**********************************');
