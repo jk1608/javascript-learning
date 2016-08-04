65b27245230ba3fc8decbff895e3e3cc1e01310c
Konduru, Jayasekhar <jayasekhar.konduru@cdk.com>
2016-08-04 12:29:29 +0530

    undefined

---
+++ ch-1/null.js
@@ -0,0 +1,19 @@
+/**
+ * Created by kondurj on 8/4/16.
+ */
+
+var x = null;
+
+
+console.log('**********************************');
+if(x) {
+    console.log('null evaluates to TRUE');
+} else {
+    console.log('null evaluates to FALSE');
+}
+console.log(null+40);
+console.log(null*40); // null evaluates to 0
+console.log(x+40);
+console.log(x*40);
+console.log(x);
+console.log('**********************************');
